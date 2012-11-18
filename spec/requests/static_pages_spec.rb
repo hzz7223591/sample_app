cd31d03309673cd82ec1bf19bee432c0f47a7af7
hzz7223591 <317603714@qq.com>
2012-11-18 21:46:21 +0800

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,21 +1,27 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  subject {page}
   describe "Home page" do
-    it  "should have the h1 'Sample App' " do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      page.should have_selector('h1',:text=>'Sample App')
+   before{visit root_path }
+      it {should have_selector('h1',:text=>'Sample App')}
+      it {should have_selector('title',:text=>full_title(''))}
+      it {should_not have_selector('title',:text=>'| Home')}
     end
-    it  "should have the content 'base title' " do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      page.should have_selector('title',:text=>'Ruby on Rails Tutorial Sample App')
-      end
-      it  "should not have a custom page title " do
-        # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-        visit '/static_pages/home'
-        page.should_not have_selector('title',:text=>'| Home')
-      end
-    end
- end
+
+  describe "Help page" do
+    before{visit help_path }
+    it {should have_selector('h1',:text=>'Help')}
+    it {should have_selector('title',:text=>full_title('Help'))}
+  end
+  describe "About page" do
+    before{visit about_path }
+    it {should have_selector('h1',:text=>'About')}
+    it {should have_selector('title',:text=>full_title('About Us'))}
+  end
+  describe "Contact page" do
+    before{visit contact_path }
+    it {should have_selector('h1',:text=>'Contact')}
+    it {should have_selector('title',:text=>full_title('Contact'))}
+  end
+  end
